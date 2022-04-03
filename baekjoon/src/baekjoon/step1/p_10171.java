c473f71cadf9ad0095079187aa23f54bdf7a1189
osj950329 <dhtkdwnsdi@gmail.com>
2022-04-03 13:59:41 +0900

    2022-04-03 commit

---
+++ baekjoon/src/baekjoon/step1/p_10171.java
@@ -0,0 +1,12 @@
+package baekjoon.step1;
+
+public class p_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
