5d959ae3e716c9721c914c8655d1d053528f2abb
osj950329 <dhtkdwnsdi@gmail.com>
2022-05-17 23:55:46 +0900

    2022-05-17 commit

--- baekjoon/src/baekjoon/step1/p_1000.java
+++ baekjoon/src/baekjoon/step1/p_1000.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 
 public class p_1000 {
 	public static void main(String[] args) {
-			Scanner s = new Scanner(System.in);
-			
-			int a = s.nextInt();
-			int b = s.nextInt();
-			
-			System.out.println(a+b);
+		Scanner sc = new Scanner(System.in);
+		int now = sc.nextInt();
+
+//		1ÀÏ ÂÉ°³±â
+//		for(int i=0; i<288; i++) {
+//			String hh = String.format("%02d", (int) ((5*i)/60));
+//			String mm = String.format("%02d",  (5*i)%60);
+//			String realLogTime = now + hh + mm;
+//			System.out.println(realLogTime);
+//		}
+
+//		6½Ã°£ ÂÉ°³±â
+		for(int i=0; i<72; i++) {
+			int hh = (5*i)/60;
+			String mm = String.format("%02d",  (5*i)%60);
+			String realLogTime = (now + hh) + mm;
+			System.out.println(realLogTime);
+		}
 	}
 }	
